cb9b02ca34b1b0a250a245382bc7e9b41978add9
Keith Chambers <kd.chambers97@gmail.com>
2019-07-17 22:29:55 +0100

    Redesigned AboutMe section

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,31 +1,36 @@
 <template>
     <div>
         <div id="aboutMe_root">
-        <header id="aboutMeHeading">A Little About Me</header>
-        <h5 id="educationHeading">Education</h5>
-        <div id="educationContent" class="aboutMeContent">
-            <p>BSc in Computer Science from Technical University of Dublin</p>
-            <br/>
-            <p>Formerly Dublin Institute of Technology (DIT)</p>
-        </div>
-        <h5 id="programmingLanguageHeading">Top Programming Languages</h5>
-        <div id="programmingLanguageContent" class="flexList aboutMeContent">    
-            <span>JavaScript</span>
-            <span>HTML5 & CSS3</span>
-            <span>SQL</span>
-            <span>C/C++</span>
-            <span>Java</span>
-            <span>Zig</span>
-        </div>
-        <h5 id="additionalSKillsHeading">Additional Skills</h5>
-        <div id="additionalSkillsContent" class="flexList aboutMeContent">
-            <span>Linux</span>
-            <span>UI/UX</span>
-            <span>Data Oriented Design</span>
+            <header id="heading">ABOUT ME</header>
+            <div id="contentContainer">
+                <div class="aboutMeInfoColumn" id="edu">
+                    <h5 class="aboutMeInfoHeading">EDUCATION</h5>
+                    <ul class="aboutMeInfoList">
+                        <li>BSc in Computer Science from Technical University of Dublin</li>
+                        <li><br/></li>
+                        <li>(Formerly DIT Kevin St)</li>
+                    </ul>
+                </div>
+                <div class="aboutMeInfoColumn" id="skills">
+                    <h5 class="aboutMeInfoHeading">SKILLS</h5>
+                    <ul class="aboutMeInfoList">
+                        <li>Web Developement</li>
+                        <li>Linux / Windows App Development</li>
+                        <li>UI Design</li>
+                    </ul>
+                </div>
+                <div class="aboutMeInfoColumn" id="projects">
+                    <h5 class="aboutMeInfoHeading">PROJECTS</h5>
+                    <ul class="aboutMeInfoList">
+                        <li>C++ Transpiler (C++)</li>
+                        <li>3D Terrain Generator (Unity)</li>
+                        <li>Money Management App (Android)</li>
+                        <li>Auction System Server (Java)</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-
 </template>
 
 <script>
@@ -34,78 +39,73 @@
     }
 </script>
 
-<style>
+<style scoped>
     
-#aboutMe_root {
-  display: grid;
-  margin: 0px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  justify-items: center;
-  align-items: flex-start;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 150px 60px 1fr;
-  grid-template-areas: 
-      ". aboutMeHeading ." 
-      "educationHeading programmingLanguageHeading additionalSkillsHeading" 
-      "educationContent programmingLanguageContent additionalSkillsContent";
-}
-
-@media only screen and (max-width: 600px) {
-  #aboutMe {
-    grid-template-columns: 1fr;
-    grid-template-rows: 150px repeat(3, 60px 1fr);
-    grid-template-areas: "aboutMeHeading" "educationHeading" "educationContent" "programmingLanguageHeading" "programmingLanguageContent" "additionalSkillsHeading" "additionalSkillsContent";
-  }
-}
+    #aboutMe_root {
+        display: grid;
+        grid-template-columns: 1fr minmax(min-content, 1000px) 1fr;
+        grid-template-areas: 
+            ". heading ."
+            ". content .";
+    }
     
-.flexList {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  align-content: center;
-}
+    #aboutMe_root * {
+/*        border: 1px solid yellow;*/
+    }
 
-.aboutMeContent {
-  font-size: 14;
-  font-family: "Roboto", sans-serif;
-  max-width: 20em;
-}
+    #contentContainer {
+        grid-area: content;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        justify-content: center;
+    }
+    
+    h5, ul, li {
+        margin: 0px;
+        padding: 0px;
+    }
+    
+    .aboutMeInfoList > li{
+        list-style: none;
+        text-align: center;
+    }
+    
+    .aboutMeInfoHeading {
+        margin-bottom: 50px;
+        font-family: "Roboto", sans-serif;
+        
+    }
+    
+    .aboutMeInfoColumn {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-end;
+        padding: 5px;
+        
+        width: 300px;
+        margin-bottom: 50px;
+    }
 
-#aboutMeHeading {
-  grid-area: aboutMeHeading;
-  padding-top: 3em;
-  font-size: 18;
-  font-weight: bold;
-  font-family: "Roboto", sans-serif;
-}
+    .aboutMeInfoColumn * {
+        font-size: 14;
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+    }
 
-#educationHeading {
-  grid-area: educationHeading;
-}
+    #heading {
+        grid-area: heading;
+        padding-bottom: 80px;
+        padding-top: 80px;
+        text-align: center;
+        font-family: "Roboto", sans-serif;
+        font-size: 20px;
+        font-weight: 400;
+        color: black;
+    }
 
-#educationContent {
-  grid-area: educationContent;
-  padding-top: 20px;
-  text-align: center;
-}
 
-#programmingLanguageHeading {
-  grid-area: programmingLanguageHeading;
-}
-
-#programmingLanguageContent {
-  grid-area: programmingLanguageContent;
-  padding-top: 20px;
-}
-
-#additionalSKillsHeading {
-  grid-area: additionalSkillsHeading;
-}
-
-#additionalSkillsContent {
-  grid-area: additionalSkillsContent;
-  padding-top: 20px;
-}
     
 </style>
